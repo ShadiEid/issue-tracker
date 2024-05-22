8aa27d62da50af1e7c1668f40deae4199ce70a47
shadieid <shadi.eid@live.com>
2024-05-22 13:02:34 +0300

    Add loading skeletons

---
+++ app/issues/IssueActions.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
+
+const IssueActions = () => {
+  return (
+    <div>
+      <Button>
+        <Link href={"/issues/new"}>Create new issue</Link>
+      </Button>
+    </div>
+  );
+};
+
+export default IssueActions;
