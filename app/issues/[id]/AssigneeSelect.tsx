5039b91a738c2e5f054f9e7e3e0b5919bec30c05
shadieid <shadi.eid@live.com>
2024-06-04 12:57:33 +0300

    Papulate the assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,15 +1,33 @@
 "use client";
 
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>();
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const users = (await axios.get<User[]>("/api/users")).data;
+      setUsers(users);
+    };
+
+    getUsers();
+  }, []);
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assignee..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">Shadi</Select.Item>
+          {users?.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
