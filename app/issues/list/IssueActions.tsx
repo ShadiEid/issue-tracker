46ce2707d9ed0e84a876593d649e3737e6fa75e8
shadieid <shadi.eid@live.com>
2024-06-24 15:12:59 +0300

    Build the filter compnent

--- app/issues/list/IssueActions.tsx
+++ app/issues/list/IssueActions.tsx
@@ -1,13 +1,15 @@
-import { Button } from "@radix-ui/themes";
+import { Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
+import IssueStatusFilter from "./IssueStatusFilter";
 
 const IssueActions = () => {
   return (
-    <div>
+    <Flex justify={"between"}>
+      <IssueStatusFilter />
       <Button>
         <Link href={"/issues/new"}>Create new issue</Link>
       </Button>
-    </div>
+    </Flex>
   );
 };
 
