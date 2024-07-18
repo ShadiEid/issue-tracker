3e244894e3a904350dd4d451321b21b83adb2b67
shadieid <shadi.eid@live.com>
2024-07-18 12:54:18 +0300

    Build the issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
 import { Flex } from "@radix-ui/themes";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
 
-export default function Home() {
-  return <LatestIssues />;
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+
+  return (
+    <Flex align={"start"} gap={"3"}>
+      <LatestIssues />
+      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+    </Flex>
+  );
 }
